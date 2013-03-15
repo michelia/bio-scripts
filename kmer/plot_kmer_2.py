de69b124e61ba5f9dfca4480724aa36f0014424b
michelia <michelia@SCGene.(none)>
2013-03-15 17:13:53 +0800

    修改了 汇总 kmer的画图程序

--- kmer/plot_kmer_2.py
+++ kmer/plot_kmer_2.py
@@ -13,19 +13,33 @@ def plot_fun(dataDir, outPic):
     
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-    colors = ['#DC143C','#8B008B','#00008B','#483D8B','#00FFFF','#008080','#9400D3','#FF00FF','#66CDAA','#008000']
+    # colors = ['#DC143C','#8B008B','#00008B','#483D8B','#00FFFF','#008080','#9400D3','#FF00FF','#66CDAA','#008000']
     colors = ['#8B0000', '#FF8C00', '#008000', '#ADFF2F', '#0000CD', '#000000', '#DC143C', '#E9967A', '#808080', '#FF00FF',]
     files = sorted(path(dataDir).files('*.histo'))
+    labels = ('1/2', '1/4')
     for i, dataFile in enumerate(files):
         filename = path(dataFile).name
         num = re.findall(r'\d+', filename)[0]
         x, y = get_data(dataFile)
-        ax.plot(x, y, linestyle='-', marker='.', label=num, c=colors[i])
+        tx, ty = get_max(x, y)
+        ax.plot(tx, ty, marker='s', ms=7)
+        print tx, ty
+        ax.annotate(str(tx), xy=(tx, ty), 
+                xytext=(tx+5, ty+1000000),
+                arrowprops=dict(fc='black', shrink=0.15, width=0.7, headwidth=0.9))
+        ax.plot(x, y, linestyle='-', marker='.', label=labels[i], c=colors[i])
     ax.legend()
     ax.set_xlabel('Depth')
     ax.set_ylabel('Frequency')
     ax.set_xlim(0, 80)
+    ax.set_ylim(0, 3*10**7)
     fig.savefig(outPic)
+
+def get_max(x, y):
+    ymax = max(y[3:])
+    ymaxX = y.index(ymax) + 1
+    return ymaxX, ymax
+
 
 def get_data(dataFile):
     reader = CsvReader(dataFile).reader
